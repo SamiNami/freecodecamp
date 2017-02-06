d248e88e8521f67b4bf5a1c9b3b009a18159596e
Sami Peralahti <sami.peralahti@gmail.com>
2017-02-06 20:01:12 +0200

    started intermedieate algorithms

---
+++ IntermediateAlgorithmScripting/sumofallnumbers.js
@@ -0,0 +1,11 @@
+// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
+function sumAll(arr) {
+  arr = Math.max(arr);
+
+
+
+  console.log(arr);
+}
+
+sumAll([1, 4]);
