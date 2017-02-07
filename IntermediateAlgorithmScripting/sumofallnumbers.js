42c3798d0eaa387fe0ccfce70d68d48ea9df1921
Sami Peralahti <sami.peralahti@gmail.com>
2017-02-07 12:04:55 +0200

    completed sum of all numbers

--- IntermediateAlgorithmScripting/sumofallnumbers.js
+++ IntermediateAlgorithmScripting/sumofallnumbers.js
@@ -1,11 +1,20 @@
 // We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
 
 function sumAll(arr) {
-  arr = Math.max(arr);
+// get the max and min value from the array, and create a new array
+  var max = Math.max.apply(null, arr);
+  var min = Math.min.apply(null, arr);
+  var result = [];
+// from min to max push a int into the array
+while (min <= max){
+  result.push(min);
+  min++;
+}
+//calculate the sum of all values in the array
+return result.reduce(function(a,b){
+  return a + b;
+}, 0 );
 
-
-
-  console.log(arr);
 }
 
 sumAll([1, 4]);
