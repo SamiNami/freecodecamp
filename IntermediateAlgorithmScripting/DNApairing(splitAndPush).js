480397f5131defc2d8062fcc39a67ef7a3a7f9dd
Sami Peralahti <sami.peralahti@gmail.com>
2017-02-09 14:46:21 +0200

    finnished DNApairing

---
+++ IntermediateAlgorithmScripting/DNApairing(splitAndPush).js
@@ -0,0 +1,28 @@
+// Match the letter with the mathcing dna pair
+
+function pairElement(str) {
+
+var dna = [];
+var arr = str.split("");
+console.log(arr);
+
+arr.forEach(function(element){
+  if(element === "G"){
+    dna.push(["G","C"]);
+  }
+  else if(element === "C"){
+    dna.push(["C","G"]);
+  }
+  else if(element === "A"){
+    dna.push(["A","T"]);
+  }
+  else if(element === "T"){
+    dna.push(["T","A"]);
+  }
+});
+return dna;
+}
+
+pairElement("GCG");
+
+// looked up another solution, could have used a switch
