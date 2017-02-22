e7781a75f8739d29fd63512b75c7568c6c51eb05
Sami Peralahti <sami.peralahti@gmail.com>
2017-02-22 17:15:22 +0200

    finished js on pomodoro, still needs comments and cleaning

--- AdvancedFrontEndDevelipmentProjects/js/pomodoro.js
+++ AdvancedFrontEndDevelipmentProjects/js/pomodoro.js
@@ -1,12 +1,82 @@
 
 $(document).ready(function(){
 
-  console.log("kappa");
+var countdown = parseInt( $(".cd").html() );
+var seconds = 0;
+var interval;
 
-var countdown = $( ".cd").val();
-var breakt = 0;
+// Click Events
 
+// display the countdown
+$(".countdown").text(countdown);
 console.log(countdown);
+
+$(".cd+").click(function(){
+  countdown = countdown + 1;
+  $(".countdown").text(countdown);
+  $(".cd").text(countdown);
+  console.log(countdown);
+  });
+
+$(".cd-").click(function(){
+  countdown = countdown - 1;
+  // prevents 0 minutes
+  if(countdown === 0){
+    countdown = 1;
+  }
+  $(".countdown").text(countdown);
+  $(".cd").text(countdown);
+});
+
+$(".reset").click(function(){
+  countdown = 25;
+  seconds = 0;
+  stopCountdown();
+
+  $(".countdown").text(countdown);
+  $(".cd").text(countdown);
+});
+
+// clicking the area starts / stops the countdown
+$(".countdownarea").click(function(){
+  if($(".start").text() === "Start" ){
+    $(".start").text("Pause");
+    startCountdown();
+  }
+  else{
+    $(".start").text("Start");
+    stopCountdown();
+  }
+});
+
+// Functions
+
+function startCountdown() {
+  interval = setInterval(whatToDo, 1000);
+}
+
+
+function whatToDo() {
+  console.log(seconds);
+  console.log(countdown);
+  seconds = seconds -1;
+  
+  if(countdown === 0 && seconds === 0){
+      window.alert("Time to take a break!");
+      $(".countdown").text(countdown + " : " + seconds);
+      stopCountdown();
+  }
+  else if(seconds === -1){
+    countdown = countdown -1;
+    seconds = 59;
+  }
+  $(".countdown").text(countdown + " : " + seconds);
+
+}
+
+function stopCountdown() {
+  clearInterval(interval);
+}
 
 
 });
