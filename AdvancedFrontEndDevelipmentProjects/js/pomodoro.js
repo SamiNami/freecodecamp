c8cbedfee964e9a4648a85a8c1f0389df4f5ea89
Sami Peralahti <sami.peralahti@gmail.com>
2017-02-23 14:36:52 +0200

    cleaned up the js code

--- AdvancedFrontEndDevelipmentProjects/js/pomodoro.js
+++ AdvancedFrontEndDevelipmentProjects/js/pomodoro.js
@@ -1,32 +1,33 @@
 
 $(document).ready(function(){
 
-var countdown = parseInt( $(".cd").html() );
-var seconds = 0;
-var interval;
+  var countdown = parseInt( $(".cd").html() );
+  var seconds = 0;
+  var interval;
+  // amount of total seconds
+  var total;
+  // varible for the current amount of seconds
+  var current = 0;
+  // keeps track if the progress-bar should be cleared
+  var clearTotal = true;
+  // keeps track if you can add or substract time
+  var preventClick = false;
 
-// amount of total seconds
-var total;
-// varible for the current amount of seconds
-var current = 0;
+  // display the countdown
+  $(".countdown").text(countdown);
 
-var clearTotal = true;
-var preventClick = false;
-// Click Events
+  // Click Events
 
-// display the countdown
-$(".countdown").text(countdown);
-
-
-
+    // adds time to the coutndown
   $(".cd+").click(function(){
+    // does not fire unless the clock has been reset
     if(preventClick === false){
     countdown = countdown + 1;
     $(".countdown").text(countdown);
     $(".cd").text(countdown);
     seconds = 0;
     }
-    });
+  });
 
   $(".cd-").click(function(){
     if(preventClick === false){
@@ -39,81 +40,77 @@ $(".countdown").text(countdown);
     $(".cd").text(countdown);
     seconds = 0;
     }
-    });
+  });
 
-
-$(".reset").click(function(){
-  countdown = 25;
-  seconds = 0;
-  stopCountdown();
-  clearTotal = true;
-  preventClick = false;
-
-  total = countdown * 60;
-  current =0;
-  $(".start").text("Start");
-
-  $(".countdown").text(countdown);
-  $(".cd").text(countdown);
-});
-
-// clicking the area starts / stops the countdown
-$(".countdownarea").click(function(){
-  if($(".start").text() === "Start" ){
-    $(".start").text("Pause");
-    startCountdown();
-    preventClick = true;
-  }
-  else{
-    $(".start").text("Start");
+  // stops and resets the clock
+  $(".reset").click(function(){
+    countdown = 25;
+    seconds = 0;
     stopCountdown();
-  }
-});
-
-// Functions
-
-function startCountdown() {
-  interval = setInterval(whatToDo, 1000);
-  // sets the total value, so it can be divided to check progress
-  if(clearTotal === true){
+    clearTotal = true;
+    preventClick = false;
     total = countdown * 60;
-  }
-}
+    current =0;
 
-function whatToDo() {
+    $(".start").text("Start");
+    $(".countdown").text(countdown);
+    $(".cd").text(countdown);
+  });
 
-  setProgress();
-
-  seconds = seconds -1;
-  if(countdown === 0 && seconds === 0){
+  // clicking the area starts / stops the countdown
+  $(".countdownarea").click(function(){
+    if($(".start").text() === "Start" ){
+      $(".start").text("Pause");
+      startCountdown();
+      preventClick = true;
+    }
+    else{
+      $(".start").text("Start");
       stopCountdown();
-      window.alert("Time to take a break!");
+    }
+  });
+
+  // Functions
+
+  function startCountdown() {
+    interval = setInterval(whatToDo, 1000);
+    // clears the total value of the progress-bar, if reset has been clicked
+    if(clearTotal === true){
+      total = countdown * 60;
+    }
   }
-  else if(seconds === -1){
-    countdown = countdown -1;
-    seconds = 59;
+
+  // what the countdown does every 1 seconds
+  function whatToDo() {
+    setProgressBar();
+    seconds = seconds -1;
+    if(countdown === 0 && seconds === 0){
+        stopCountdown();
+        window.alert("Time to take a break!");
+    }
+    else if(seconds === -1){
+      countdown = countdown -1;
+      seconds = 59;
+    }
+    $(".countdown").text(countdown + " : " + seconds);
   }
-  $(".countdown").text(countdown + " : " + seconds);
+  // stops the countdown
+  function stopCountdown() {
+    clearInterval(interval);
+    clearTotal = false;
+  }
 
-}
+  // calulates and displays the value for the progress-bar
+  function setProgressBar() {
+      current += 1;
+      var progress = (current/ total) * 100;
+      progress = roundToTwo(progress);
+      $('.progress-bar').attr('style','width:' +progress+ '%');
+  }
 
-function stopCountdown() {
-  clearInterval(interval);
-  clearTotal = false;
-}
-
-// sets the progress of the bar
-function setProgress() {
-    current += 1;
-    var progress = (current/ total) * 100;
-    progress = roundToTwo(progress);
-    $('.progress-bar').attr('style','width:' +progress+ '%');
-}
-// rounds to Two numbers
-function roundToTwo(num) {
-    return +(Math.round(num + "e+2")  + "e-2");
-}
-
-
+  // rounds to two decimals
+  function roundToTwo(num) {
+      return +(Math.round(num + "e+2")  + "e-2");
+  }
 
 });
