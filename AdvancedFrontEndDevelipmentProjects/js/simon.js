35cc33c2b4f6c9012551deb7cdfd228759a9d532
Sami Peralahti <sami.peralahti@gmail.com>
2017-03-09 15:03:54 +0200

    for simon, added the sequence and checks for if you press the wrong seqence

--- AdvancedFrontEndDevelipmentProjects/js/simon.js
+++ AdvancedFrontEndDevelipmentProjects/js/simon.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 
 var canClick = false;
 var sequence = [];
+var playerSequence = [];
+// the step the player is on
+var pStep = 0;
+
 
 // set's the value of canClick, uses the toggle button
 $(function setClick() {
@@ -19,41 +23,116 @@ $(function setClick() {
   });
 });
 
+// what the stat button does
 $(".start").click(function(){
+  // only works if the toggle is switched on
   if(canClick){
-  // add clear();
+  // reset the sequence
+  clear();
+  addStepSqeuence();
+  showSequence();
+  }
+});
 
+function addStepSqeuence(){
   // get a random number between 1-4
   var number = Math.floor((Math.random() * 4) + 1);
   sequence.push(number);
-  console.log(sequence);
-
-  showSequence();
-
-  }
-
-});
+}
 
 function showSequence(){
-  // prevents clicking during sequence
-  canClick = false;
+var i = 0;
+myLoop(i, sequence);
+}
 
-  sequence.forEach(function(target){
+// loop used to show
+function myLoop (i, arr) {
+   setTimeout(function () {
+     turnOn(i, arr);
+     playSound(i, arr);
+     turnOff(i, arr);
+      i++;
+      if (i < arr.length) {
+         myLoop(i, arr);
+      }
+   }, 700);
+}
 
-    $('#'+ target).css("opacity","1.0");
-    document.getElementById('sound' + target).play();
+// turns the light on
+function turnOn(i, arr){
+  $('#'+ arr[i]).css("opacity","1.0");
+}
 
-    setTimeout(function(){
-      $('#'+ target).css("opacity","0.5");
-    }, 500);
+// turns the light off after 500ms
+function turnOff(i, arr){
+  setTimeout(function () {
+  $('#'+ arr[i]).css("opacity","0.5");
+  }, 500);
+}
 
-
-
-  });
-
+// plays the sound...
+function playSound(i, arr){
+  document.getElementById('sound' + arr[i]).play();
 }
 
 
+function clear(){
+   sequence = [];
+   playerSequence = [];
+   pStep = 0;
+}
+
+
+$("#1,#2,#3,#4").click(function(){
+  if(canClick){
+
+
+    var buttonNumber = parseInt(this.id);
+/*
+    turnOn(buttonNumber, sequence);
+    playSound(buttonNumber, sequence);
+    turnOff(buttonNumber, sequence);
+*/
+    playerSequence.push(buttonNumber);
+    // compare that the current step is the same as in the sequence
+    checkStep();
+
+    if(arraysEqual(playerSequence, sequence)){
+        playerSequence = [];
+        pStep = 0;
+        addStepSqeuence();
+        showSequence();
+      }
+
+
+
+  }
+});
+
+function checkStep(){
+
+  var check = sequence.slice(pStep, pStep + 1);
+
+  if(check.indexOf(playerSequence[pStep]) !== 0){
+        playerSequence = [];
+        pStep = 0;
+        showSequence();
+    }
+  else{
+    pStep++;
+  }
+
+}
+
+function arraysEqual(arr1, arr2){
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;){
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+    return true;
+}
 
 
 
