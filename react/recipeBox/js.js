c97f2f7db5546ac6a085c9da3488eb6445cf479c
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-26 19:10:49 +0300

    modal popup, adding objects to state, etc

--- react/recipeBox/js.js
+++ react/recipeBox/js.js
@@ -1,5 +1,7 @@
-
-
+var Button = ReactBootstrap.Button;
+var Modal = ReactBootstrap.Modal;
+var FormGroup = ReactBootstrap.FormGroup;
+var FormControl = ReactBootstrap.FormControl;
 
 class Box extends React.Component {
 
@@ -19,24 +21,110 @@ class Box extends React.Component {
 
 
   }
+
+  updateRecipeBook(title,ingreedients) {
+    let recipeBook = this.state.recipeBook.slice();
+
+    const newObj = {
+      title: title,
+      ingreedients: ingreedients
+    }
+
+    recipeBook.push(newObj);
+    this.setState({recipeBook});
+
+    console.log(recipeBook);
+  }
+
   render() {
 
     return (
 			 <div className="container">
 
-					<div className="row card">
-            <div className="">{this.state.recipeBook[0].title}</div>
+					<div className="well well">
+               {this.state.recipeBook.map((recipe) =>
+                <div>{recipe.title}</div>)}
 					</div>
 
-
         <div className="row">
-          <button type="button" className="btn"   onClick={() => { this.props.getData("alltime") }}> Add Rescipe</button>
+          <ModalButton updateRecipeBook={this.updateRecipeBook.bind(this)}/>
         </div>
       </div>
     )
   }
-
 }
+
+const ModalButton = React.createClass({
+
+
+  getInitialState() {
+    return { showModal: false };
+  },
+
+  close() {
+    this.setState({ showModal: false });
+  },
+
+  open() {
+    this.setState({ showModal: true });
+  },
+
+  render() {
+
+    return (
+      <div>
+
+        <Button
+          bsStyle="primary"
+          bsSize="large"
+          onClick={this.open}
+        >
+          Add Recipe
+        </Button>
+
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Recipe</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+
+            <FormGroup>
+              <ReactBootstrap.ControlLabel>Add Recipe</ReactBootstrap.ControlLabel>
+              <FormControl type="text" placeholder="Recipe Name" ref="title"/>
+            </FormGroup>
+
+
+            <FormGroup>
+              <ReactBootstrap.ControlLabel>Ingredients</ReactBootstrap.ControlLabel>
+              <FormControl componentClass="textarea" placeholder="Enter ingredients separated by comma" ref="ingreedients"/>
+            </FormGroup>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="primary" onClick={() => {this.handleClick()} }>Add Recipe</Button>
+            <Button onClick={this.close}>Cancel</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  },
+  getFormControlNodeTitle: function() {
+    // Get the underlying <input> DOM element
+    return ReactDOM.findDOMNode(this.refs.title);
+  },
+  getFormControlNodeIngreedients: function() {
+    return ReactDOM.findDOMNode(this.refs.ingreedients);
+  },
+  handleClick: function(){
+    const title = this.getFormControlNodeTitle().value;
+    const ingreedients = this.getFormControlNodeIngreedients().value.split(",");
+    this.props.updateRecipeBook(title,ingreedients);
+    this.setState({ showModal: false });
+  }
+});
+
+
+
 
 
 ReactDOM.render(
